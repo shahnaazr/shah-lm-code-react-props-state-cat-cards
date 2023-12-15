bb628f6291b7a147948fd510ae5ef6c51fc5d0fe
Shahnaaz Rahamatullah <rahamsha@gmail.com>
2023-12-15 16:42:11 +0000

    feat: add props to cat card component

--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -1,10 +1,23 @@
-const CatCard: React.FC = () => (
-  <div className="card">
-    <h3 className="card__text card__header">Mr Gwumpy</h3>
-    <p className="card__text">Species: Cat</p>
-    <p className="card__text">Favourite Food(s): Caviar</p>
-    <p className="card__text">Birth Year: 1984</p>
-  </div>
-);
+interface CatCardProps {
+  name: string;
+  species: string;
+  favFoods: Array<string>;
+  birthYear: number;
+}
+
+const CatCard: React.FC<CatCardProps> = (props) => {
+  console.log(
+    "CatCard receiving props from App?! Come in App?! 😸 Props received are: ",
+    props
+  );
+  return (
+    <div className="card">
+      <h3 className="card__text card__header">{props.name}</h3>
+      <p className="card__text">Species: {props.species}</p>
+      <p className="card__text">Favourite Food(s): {props.favFoods}</p>
+      <p className="card__text">Birth Year: {props.birthYear}</p>
+    </div>
+  );
+};
 
 export default CatCard;
